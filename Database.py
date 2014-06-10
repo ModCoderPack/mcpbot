55caf0aa7793676ee811ddabd95449f1d7941fcc
ProfMobius <devnull@localhost>
2014-06-10 20:38:45 +0200

    Added database support Added MCPBot class Basic connection and SQL command handling

---
+++ Database.py
@@ -0,0 +1,34 @@
+import psycopg2
+import Logger
+
+class Database(object):
+    def __init__(self, host, port, user, db, pwd, bot):
+
+        self.bot  = bot
+        self.host = host
+        self.port = port
+        self.user = user
+        self.db   = db
+        self.pwd  = pwd
+
+        self.conn   = None
+        self.cursor = None
+
+        self.logger = Logger.getLogger("MCPBot.Database", self.bot.lognormal, self.bot.logerrors)
+
+    def connect(self):
+        self.conn = psycopg2.connect(database=self.db, user=self.user, password=self.pwd, host=self.host, port=self.port)
+        if not self.conn:
+            self.logger.error("Error while connecting to database")
+            return None
+        else:
+            self.logger.info("Connection to database established")
+            self.cursor = self.conn.cursor()
+            return self.cursor
+
+    def execute(self, request):
+        self.cursor.execute(request)
+        retval = self.cursor.fetchall()
+        self.conn.commit()
+
+        return retval
--- MCPBot.py
+++ MCPBot.py
@@ -1,4 +1,31 @@
 from BotBase import BotBase
+from Database import Database
+import Logger
 
-bot = BotBase()
-bot.run()
+class MCPBot(BotBase):
+    def __init__(self):
+        super(MCPBot, self).__init__()
+        self.logger = Logger.getLogger("MCPBot", self.lognormal, self.logerrors)
+        self.db     = Database('172.245.30.34', 5432, 'postgres', 'mcpbot', 'MCPBot0', self)
+
+        self.registerCommand('sqlrequest', self.sqlrequest, ['admin'], 1, 999, "Execute a raw SQL command")
+
+    def run(self):
+        self.db.connect()
+        self.connect()
+
+    def sqlrequest(self, bot, sender, dest, cmd, args):
+        sql = ' '.join(args)
+        val = self.db.execute(sql)
+
+        for entry in val:
+            self.sendNotice(sender.nick, str(entry))
+
+########################################################################################################################
+def main():
+    bot = MCPBot()
+    bot.run()
+    BotBase.startBots()
+
+if __name__ == "__main__":
+    main()
