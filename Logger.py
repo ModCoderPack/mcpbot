660520481a2ead5af70138652dea8e22a6670ec6
bspkrs <devnull@localhost>
2014-07-31 13:37:02 -0400

    changed logger to only add handlers if there are not already handlers defined

--- Logger.py
+++ Logger.py
@@ -3,22 +3,23 @@ import logging
 def getLogger(name, lognormal='botlog.log', logerror='errors.log'):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)10s - %(message)s')
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)20s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
 
-    fh = logging.FileHandler(logerror)
-    fh.setLevel(logging.WARNING)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    if len(logger.handlers) == 0:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)20s - %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
-    nh = logging.FileHandler(lognormal)
-    nh.setLevel(logging.DEBUG)
-    nh.setFormatter(formatter)
-    logger.addHandler(nh)
+        fh = logging.FileHandler(logerror)
+        fh.setLevel(logging.WARNING)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+        nh = logging.FileHandler(lognormal)
+        nh.setLevel(logging.DEBUG)
+        nh.setFormatter(formatter)
+        logger.addHandler(nh)
 
     return logger
 
