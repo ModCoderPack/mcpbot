14ae04ba78365825c1d33971dba3c389c3f4dc6e
bspkrs <devnull@localhost>
2014-08-22 13:43:13 -0400

    added commands to list unnamed members in a given class

--- MCPBot.py
+++ MCPBot.py
@@ -38,9 +38,9 @@ class MCPBot(BotBase):
         self.registerCommand('findallf', self.findAllKey, ['any'], 1, 2, "<regex pattern> [<version>]",             "Returns field entries matching a regex pattern. Allows partial matches to be returned.")
         self.registerCommand('findallm', self.findAllKey, ['any'], 1, 2, "<regex pattern> [<version>]",             "Returns method entries matching a regex pattern. Allows partial matches to be returned.")
         self.registerCommand('findallp', self.findAllKey, ['any'], 1, 2, "<regex pattern> [<version>]",             "Returns parameter entries matching a regex pattern. Allows partial matches to be returned.")
-        self.registerCommand('lf',       self.listMembers,['any'], 1, 2, "<class>",                                 "Returns a list of unmapped fields for a given class. Use DCC if the list is long.")
-        self.registerCommand('lm',       self.listMembers,['any'], 1, 2, "<class>",                                 "Returns a list of unmapped methods for a given class. Use DCC if the list is long.")
-        self.registerCommand('lp',       self.listMembers,['any'], 1, 2, "<class>",                                 "Returns a list of unmapped method parameters for a given class. Use DCC if the list is long (likely).")
+        self.registerCommand('lf',       self.listMembers,['any'], 1, 1, "<class>",                                 "Returns a list of unmapped fields for a given class. Use DCC if the list is long.")
+        self.registerCommand('lm',       self.listMembers,['any'], 1, 1, "<class>",                                 "Returns a list of unmapped methods for a given class. Use DCC if the list is long.")
+        self.registerCommand('lp',       self.listMembers,['any'], 1, 1, "<class>",                                 "Returns a list of unmapped method parameters for a given class. Use DCC if the list is long (likely).")
 
         self.registerCommand('sf',  self.setMember,  ['any'],        2, 999, "<srg name> <new name> [<comment>]", "Sets the MCP name and comment for the SRG field specified. SRG index can also be used.")
         self.registerCommand('fsf', self.setMember,  ['maintainer'], 2, 999, "<srg name> <new name> [<comment>]", "Force sets the MCP name and comment for the SRG field specified. SRG index can also be used.")
@@ -173,7 +173,19 @@ class MCPBot(BotBase):
             self.sendClassResults(sender, val, status, limit, summary=True)
 
     def listMembers(self, bot, sender, dest, cmd, args):
-        self.sendNotice(sender.nick, "+++§B Coming Soon™ §N+++")
+        if cmd['command'] == 'lf':
+            self.sendNotice(sender.nick, "+++§B UNNAMED %sS FOR %s §N+++" % ('FIELD', args[0]))
+            val, status = self.db.getUnnamed('field', args)
+            self.sendMemberResults(sender, val, status, 20, summary=True)
+        elif cmd['command'] == 'lm':
+            self.sendNotice(sender.nick, "+++§B UNNAMED %sS FOR %s §N+++" % ('METHOD', args[0]))
+            val, status = self.db.getUnnamed('method', args)
+            self.sendMemberResults(sender, val, status, 20, summary=True)
+        else:
+            self.sendNotice(sender.nick, "+++§B UNNAMED %sS FOR %s §N+++" % ('METHOD PARAM', args[0]))
+            val, status = self.db.getUnnamed('method_param', args)
+            self.sendParamResults(sender, val, status, 20, summary=True)
+
 
     # Setters
 
@@ -245,7 +257,7 @@ class MCPBot(BotBase):
                     self.sendNotice(sender.nick, " ".format(**entry))
             else:
                 if i < limit or sender.dccSocket:
-                    self.sendNotice(sender.nick, "{class_srg_name}.{method_srg_name}.{srg_name} §B[§N {srg_descriptor} §B] =>§N {class_srg_name}.{method_mcp_name}.{mcp_name} §B[§N {srg_descriptor} §B]".format(**entry))
+                    self.sendNotice(sender.nick, "{class_srg_name}.{method_srg_name}.{srg_name} §B[§N {srg_descriptor} §B] =>§N {class_srg_name}.{method_mcp_name}.{mcp_name} §B[§N {java_type_code} §B]".format(**entry))
                 elif i == limit:
                     self.sendNotice(sender.nick, "§B+ §N%(count)d§B more. Please use the %(cmd_char)sdcc command to see the full list." % {'count': len(val) - i, 'cmd_char': self.cmdChar})
                     return
