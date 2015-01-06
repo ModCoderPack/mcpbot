9f48440086e3188e9641ce003db8ac2d07f7372c
bspkrs <devnull@localhost>
2015-01-06 10:58:22 -0500

    small query fix for when 1.8 is no longer "current"

--- Database.py
+++ Database.py
@@ -185,7 +185,7 @@ class Database(object):
         if len(args) > 1:
             versplit = args[1].split('.')
             if len(versplit) == 2:
-                sqlrequest += "where mcp_version_code like %(version)s "
+                sqlrequest += "where (mcp_version_code like %(version)s or mc_version_code like %(version)s) "
             else:
                 sqlrequest += "where mc_version_code like %(version)s "
         else:
