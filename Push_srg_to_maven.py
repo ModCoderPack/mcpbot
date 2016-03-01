1b31901d7ddd4e81736b50afdbeaf43bf63a00a2
bspkrs <bspkrs@gmail.com>
2016-03-01 17:09:12 -0500

    actually add srg zips to tracked files

--- Push_srg_to_maven.py
+++ Push_srg_to_maven.py
@@ -10,7 +10,7 @@ config.read(configfile)
 maven_repo_url = config.get('EXPORT', 'MAVEN_REPO_URL', 'http://files.minecraftforge.net/maven/manage/upload/de/oceanlabs/mcp/')
 maven_repo_user = config.get('EXPORT', 'MAVEN_REPO_USER', 'mcp')
 maven_repo_pass = config.get('EXPORT', 'MAVEN_REPO_PASS', '')
-mc_version = '1.8.9'
+mc_version = '1.9'
 
 print MavenHandler.upload(maven_repo_url, maven_repo_user, maven_repo_pass, 'mcp-' + mc_version + '-csrg.zip', remote_path='mcp/' + mc_version, logger=logger)
 print MavenHandler.upload(maven_repo_url, maven_repo_user, maven_repo_pass, 'mcp-' + mc_version + '-srg.zip', remote_path='mcp/' + mc_version, logger=logger)
